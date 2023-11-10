071ef272f5633f550356e6de37a43a34fa925a2a
Mauricio Figueroa <maurixfx@gmail.com>
2023-11-10 23:13:32 +0100

    Move MockAPIClient to its own file

---
+++ WorkoutsTests/Network/Mock/MockAPIClient.swift
@@ -0,0 +1,45 @@
+//
+//  MockAPIClient.swift
+//  WorkoutsTests
+//
+//  Created by Mauricio Figueroa on 10-11-23.
+//
+
+import Foundation
+@testable import Workouts
+
+final class MockAPIClient: HTTPClient {
+    private(set) var getCallsCount = 0
+    private(set) var getWasCalled = false
+    var getResult: Result<ExerciseResponse, APIError>?
+
+    func get<T>(_ url: String, responseType: T.Type) async throws -> T where T : Decodable {
+        getWasCalled = true
+        getCallsCount += 1
+        
+        let mockValue: T = try await withCheckedThrowingContinuation { continuation in
+            
+            guard let getResult else {
+                continuation.resume(throwing: APIError.decodingError)
+                return
+            }
+            
+            switch getResult {
+            case .failure(let expectedError):
+                continuation.resume(throwing: expectedError)
+            case .success(let expectedResponse):
+                do {
+                    if let expectedGenericValue = expectedResponse as? T {
+                        continuation.resume(returning: expectedGenericValue)
+                    } else {
+                        throw APIError.decodingError
+                    }
+                } catch {
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+        
+        return mockValue
+    }
+}
--- WorkoutsTests/Services/ExerciseManagerTests.swift
+++ WorkoutsTests/Services/ExerciseManagerTests.swift
@@ -75,40 +75,4 @@ final class ExerciseManagerTests: XCTestCase {
             return response.results
         }
     }
-    
-    private class MockAPIClient: HTTPClient {
-        private(set) var getCallsCount = 0
-        private(set) var getWasCalled = false
-        var getResult: Result<ExerciseResponse, APIError>?
-
-        func get<T>(_ url: String, responseType: T.Type) async throws -> T where T : Decodable {
-            getWasCalled = true
-            getCallsCount += 1
-            
-            let mockValue: T = try await withCheckedThrowingContinuation { continuation in
-                
-                guard let getResult else {
-                    continuation.resume(throwing: APIError.decodingError)
-                    return
-                }
-                
-                switch getResult {
-                case .failure(let expectedError):
-                    continuation.resume(throwing: expectedError)
-                case .success(let expectedResponse):
-                    do {
-                        if let expectedGenericValue = expectedResponse as? T {
-                            continuation.resume(returning: expectedGenericValue)
-                        } else {
-                            throw APIError.decodingError
-                        }
-                    } catch {
-                        continuation.resume(throwing: error)
-                    }
-                }
-            }
-            
-            return mockValue
-        }
-    }
 }
