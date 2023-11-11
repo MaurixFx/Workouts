8231c690534462c689fc521df417281b6dca9201
Mauricio Figueroa <maurixfx@gmail.com>
2023-11-11 11:08:13 +0100

    ExerciseListViewModel 'loadExercises'  set currentState to error when ExerciseManagerFails

--- WorkoutsTests/ViewModel/ExerciseListViewModelTest.swift
+++ WorkoutsTests/ViewModel/ExerciseListViewModelTest.swift
@@ -19,27 +19,73 @@ final class ExerciseListViewModelTest: XCTestCase {
         XCTAssertTrue(service.fetchWasCalled)
     }
     
+    func test_loadExercises_setCurrentStateToError_whenExerciseManagerFails() async {
+        let expectedError = APIError.invalidResponse
+        let service = MockExerciseManager()
+        let sut = ExerciseListViewModel(service: service)
+        service.fetchResult = .failure(expectedError)
+        
+        await sut.loadExercises()
+        
+        XCTAssertEqual(sut.currentState, .error(expectedError), "currentState should be .error when ExerciseManager fails")
+    }
+    
     private class ExerciseListViewModel {
         private let service: ExerciseService
+        
+        enum State: Equatable {
+            static func == (lhs: State, rhs: State) -> Bool {
+                switch (lhs, rhs) {
+                case (.initial, .initial):
+                    return true
+                case let (.error(error1), .error(error2)):
+                    return type(of: error1) == type(of: error2) && "\(error1)" == "\(error2)"
+                default:
+                    return false
+                }
+            }
+            
+            case initial
+            case error(Error)
+        }
+        
+        private(set) var currentState: State = .initial
         
         init(service: ExerciseService) {
             self.service = service
         }
 
         func loadExercises() async {
-            _ = try? await service.fetch()
+            do {
+                _ = try await service.fetch()
+            } catch {
+                currentState = .error(error)
+            }
         }
     }
     
     private class MockExerciseManager: ExerciseService {
         private(set) var fetchWasCalled = false
         private(set) var fetchCallsCount = 0
+        var fetchResult: Result<[Exercise], Error>?
 
         func fetch() async throws -> [Exercise] {
             fetchWasCalled = true
             fetchCallsCount += 1
             
-            return []
+            return try await withCheckedThrowingContinuation { continuation in
+                guard let fetchResult else {
+                    continuation.resume(throwing: APIError.invalidResponse)
+                    return
+                }
+                
+                switch fetchResult {
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                default:
+                    break
+                }
+            }
         }
     }
 }
