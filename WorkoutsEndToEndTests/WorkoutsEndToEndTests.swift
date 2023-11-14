b355fd1499503465a2efa75e4abee8d21152d67d
Mauricio Figueroa <maurixfx@gmail.com>
2023-11-14 13:51:02 +0100

    Create WorkoutsEndToEndTests target

---
+++ WorkoutsEndToEndTests/WorkoutsEndToEndTests.swift
@@ -0,0 +1,11 @@
+//
+//  WorkoutsEndToEndTests.swift
+//  WorkoutsEndToEndTests
+//
+//  Created by Mauricio Figueroa on 14-11-23.
+//
+
+import XCTest
+
+final class WorkoutsEndToEndTests: XCTestCase {
+}
