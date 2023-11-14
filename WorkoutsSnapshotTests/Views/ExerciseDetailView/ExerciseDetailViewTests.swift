e809fa3475faa7f489f91bb1940ebc853a6a868e
Mauricio Figueroa <maurixfx@gmail.com>
2023-11-14 14:30:46 +0100

    ExerciseDetilView looks good when exercise does not have images and variations

---
+++ WorkoutsSnapshotTests/Views/ExerciseDetailView/ExerciseDetailViewTests.swift
@@ -0,0 +1,22 @@
+//
+//  ExerciseDetailViewTests.swift
+//  WorkoutsSnapshotTests
+//
+//  Created by Mauricio Figueroa on 14-11-23.
+//
+
+import Foundation
+import XCTest
+import SnapshotTesting
+@testable import Workouts
+
+final class ExerciseDetailViewTests: XCTestCase {
+    func test_view_looksGood_whenExerciseDoesNotHaveImagesAndVariations() {
+        let exercise = Exercise(id: 1, name: "Jump Jump", description: "Jumping Jumping and dancing", images: [], variations: [])
+        let viewModel = ExerciseDetailViewModel(exercise: exercise, coordinator: ExerciseCoordinatorManager(), isVariationExerciseDetail: false)
+        
+        let view = ExerciseDetailView(viewModel: viewModel)
+
+        assertSnapshots(of: view, as: [.image(layout: .fixed(width: 375, height: 600))], record: true)
+    }
+}
